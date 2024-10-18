5630fecbe942b5cc9ca12073ff36176c10d76904
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2024-10-18 16:15:26 +0500

    Initial commit

---
+++ src/main/java/org/example/rolestask/RolesTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.rolestask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RolesTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rolestask/RolesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rolestask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
